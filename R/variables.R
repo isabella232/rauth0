d1d422177266ef81711ecc4707c60461c8ed89ca
kran <rodrigo.antonioli@auth0.com>
2020-10-23 16:34:02 +0200

    social connections vars

--- R/variables.R
+++ R/variables.R
@@ -12,6 +12,28 @@ CONST_LOGIN_TYPES=c('s', 'sepft', 'ssa', 'seoobft', 'seotpft', 'sercft', 'sertft
 #' @export
 CONST_LOGIN_TYPES_STR=paste0("('", paste(CONST_LOGIN_TYPES, collapse="', '"), "')")
 
+#' Social Connections
+#'
+#' Connections from social sources (Apple, Google, etc)
+#'
+#' @export
+CONST_SOCIAL_CONNECTIONS=c(
+  'amazon', 'apple', 'aol', 'baidu', 'bitbucket', 'box', 'dropbox', 'dwolla', 'ebay',
+  'exact', 'facebook', 'fitbit', 'github', 'google-openid', 'google-oauth2',
+  'instagram', 'linkedin', 'miicard', 'oidc', 'paypal', 'paypal-sandbox',
+  'planningcenter', 'renren', 'salesforce', 'salesforce-community',
+  'salesforce-sandbox', 'evernote', 'evernote-sandbox', 'shopify', 'soundcloud',
+  'thecity', 'thecity-sandbox', 'thirtysevensignals', 'twitter', 'vkontakte',
+  'windowslive', 'wordpress', 'yahoo', 'yammer', 'yandex', 'weibo', 'line',
+  'oauth2', 'oauth1', 'custom' ## TODO: confirm if this line counts as social
+)
+
+#' Social Connections (string)
+#'
+#' Connections from social sources (Apple, Google, etc), as a string
+#'
+#' @export
+CONST_SOCIAL_CONNECTIONS_STR=paste0("('", paste(CONST_SOCIAL_CONNECTIONS, collapse="', '"), "')")
 
 
 #' SCD Start date
