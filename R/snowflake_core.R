a928ce36298d3e150416adca901cb9939cbb9023
dm03514 <dm03514@gmail.com>
2021-01-27 10:33:47 -0500

    Snowflake COnnection fucntion

---
+++ R/snowflake_core.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(dplyr.snowflakedb)
+
+
+#' @export
+snowflake_connect = function(credentials) {
+    Sys.setenv(JAVA_HOME = credentials$javahome)
+    options(dplyr.jdbc.classpath = credentials$snowflakejdbc)
+    con = src_snowflakedb(
+        user = credentials$user,
+        password = credentials$password,
+        account = credentials$account,
+        opts = list(
+            warehouse = credentials$warehouse,
+            db = credentials$db,
+            schema = credentials$schema
+        )
+    )
+
+    con
+}
